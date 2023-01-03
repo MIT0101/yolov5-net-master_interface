3f64d786a86d213d222236b2668b5aa365b2d420
Mohammed Al Mustafa Adel <alraqym050@gmail.com>
2023-01-03 11:45:23 +0300

    inital commit

--- TEST Object Detection/Form1.Designer.cs
+++ TEST Object Detection/Form1.Designer.cs
@@ -39,24 +39,29 @@
             this.score_txt = new System.Windows.Forms.TextBox();
             this.object_txt = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.source_pbox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.result_pbox)).BeginInit();
             this.SuspendLayout();
             // 
             // source_pbox
             // 
-            this.source_pbox.Location = new System.Drawing.Point(12, 35);
+            this.source_pbox.BackColor = System.Drawing.Color.Gray;
+            this.source_pbox.Location = new System.Drawing.Point(12, 124);
             this.source_pbox.Name = "source_pbox";
-            this.source_pbox.Size = new System.Drawing.Size(427, 312);
+            this.source_pbox.Size = new System.Drawing.Size(480, 279);
             this.source_pbox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.source_pbox.TabIndex = 0;
             this.source_pbox.TabStop = false;
             // 
             // result_pbox
             // 
-            this.result_pbox.Location = new System.Drawing.Point(459, 35);
+            this.result_pbox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.result_pbox.BackColor = System.Drawing.Color.Gray;
+            this.result_pbox.Location = new System.Drawing.Point(499, 124);
             this.result_pbox.Name = "result_pbox";
-            this.result_pbox.Size = new System.Drawing.Size(427, 312);
+            this.result_pbox.Size = new System.Drawing.Size(480, 279);
             this.result_pbox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.result_pbox.TabIndex = 0;
             this.result_pbox.TabStop = false;
@@ -64,9 +69,10 @@
             // detect_btn
             // 
             this.detect_btn.Enabled = false;
-            this.detect_btn.Location = new System.Drawing.Point(381, 488);
+            this.detect_btn.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.detect_btn.Location = new System.Drawing.Point(12, 526);
             this.detect_btn.Name = "detect_btn";
-            this.detect_btn.Size = new System.Drawing.Size(132, 29);
+            this.detect_btn.Size = new System.Drawing.Size(480, 55);
             this.detect_btn.TabIndex = 1;
             this.detect_btn.Text = "Detect";
             this.detect_btn.UseVisualStyleBackColor = true;
@@ -74,9 +80,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(123, 367);
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(12, 427);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(132, 29);
+            this.button1.Size = new System.Drawing.Size(149, 39);
             this.button1.TabIndex = 1;
             this.button1.Text = "Chose file";
             this.button1.UseVisualStyleBackColor = true;
@@ -84,10 +91,12 @@
             // 
             // button2
             // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.button2.Enabled = false;
-            this.button2.Location = new System.Drawing.Point(591, 394);
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(499, 526);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(185, 29);
+            this.button2.Size = new System.Drawing.Size(480, 58);
             this.button2.TabIndex = 1;
             this.button2.Text = "Save Result Image to";
             this.button2.UseVisualStyleBackColor = true;
@@ -95,60 +104,93 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(591, 354);
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(673, 472);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(185, 27);
+            this.textBox1.Size = new System.Drawing.Size(185, 39);
             this.textBox1.TabIndex = 2;
             // 
             // label1
             // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(481, 361);
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(499, 479);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(104, 20);
+            this.label1.Size = new System.Drawing.Size(168, 32);
             this.label1.TabIndex = 3;
             this.label1.Text = "save file name";
             // 
             // label2
             // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(381, 450);
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(499, 427);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 20);
+            this.label2.Size = new System.Drawing.Size(73, 32);
             this.label2.TabIndex = 3;
             this.label2.Text = "Score";
             // 
             // score_txt
             // 
-            this.score_txt.Location = new System.Drawing.Point(439, 447);
+            this.score_txt.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.score_txt.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.score_txt.Location = new System.Drawing.Point(585, 424);
             this.score_txt.Name = "score_txt";
             this.score_txt.ReadOnly = true;
-            this.score_txt.Size = new System.Drawing.Size(82, 27);
+            this.score_txt.Size = new System.Drawing.Size(82, 39);
             this.score_txt.TabIndex = 2;
             // 
             // object_txt
             // 
-            this.object_txt.Location = new System.Drawing.Point(473, 414);
+            this.object_txt.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.object_txt.Location = new System.Drawing.Point(287, 476);
             this.object_txt.Name = "object_txt";
-            this.object_txt.Size = new System.Drawing.Size(112, 27);
+            this.object_txt.Size = new System.Drawing.Size(112, 39);
             this.object_txt.TabIndex = 2;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(308, 421);
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(12, 479);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(159, 20);
+            this.label3.Size = new System.Drawing.Size(257, 32);
             this.label3.TabIndex = 3;
             this.label3.Text = "object You Lokking For";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(138, 77);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(196, 32);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Image To Detect ";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(677, 77);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(151, 32);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Result Image";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(944, 554);
+            this.ClientSize = new System.Drawing.Size(991, 665);
             this.Controls.Add(this.label2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.score_txt);
@@ -181,5 +223,7 @@
         private TextBox score_txt;
         private TextBox object_txt;
         private Label label3;
+        private Label label4;
+        private Label label5;
     }
 }
